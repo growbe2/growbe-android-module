d089a8a7cd5570aa3601b4cae9c862b5f0a5b90a
William Quintal <william95quintalwilliam@outlook.com>
2022-08-01 19:32:40 -0400

    start a gatt server in a service

---
+++ app/src/main/java/ca/berlingoqc/growbe_android_module/GattServerService.kt
@@ -0,0 +1,152 @@
+package ca.berlingoqc.growbe_android_module
+
+import android.app.Service
+import android.bluetooth.*
+import android.bluetooth.le.AdvertiseCallback
+import android.bluetooth.le.AdvertiseData
+import android.bluetooth.le.AdvertiseSettings
+import android.bluetooth.le.BluetoothLeAdvertiser
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.os.IBinder
+import android.os.ParcelUuid
+import android.util.Log
+
+private const val TAG = "GattServerActivity"
+
+class GattServerService : Service() {
+
+    private lateinit var bluetoothManager: BluetoothManager
+    private var bluetoothGattServer: BluetoothGattServer? = null
+
+    /* Collection of notification subscribers */
+    private val registeredDevices = mutableSetOf<BluetoothDevice>()
+
+
+    private val bluetoothReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            when (intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.STATE_OFF)) {
+                BluetoothAdapter.STATE_ON -> {
+                    startAdvertising()
+                    startServer()
+                }
+                BluetoothAdapter.STATE_OFF -> {
+                    stopServer()
+                    stopAdvertising()
+                }
+            }
+        }
+    }
+
+    /**
+     * Callback to receive information about the advertisement process.
+     */
+    private val advertiseCallback = object : AdvertiseCallback() {
+        override fun onStartSuccess(settingsInEffect: AdvertiseSettings) {
+            Log.i(TAG, "LE Advertise Started.")
+        }
+
+        override fun onStartFailure(errorCode: Int) {
+            Log.w(TAG, "LE Advertise Failed: $errorCode")
+        }
+    }
+
+    private val gattServerCallback = object : BluetoothGattServerCallback() {
+        override fun onConnectionStateChange(device: BluetoothDevice?, status: Int, newState: Int) {
+            Log.i(TAG, "Connection state change. $device $status")
+            super.onConnectionStateChange(device, status, newState)
+        }
+    }
+
+
+    fun startAdvertising() {
+        val bluetoothLeAdvertiser: BluetoothLeAdvertiser? =
+            bluetoothManager.adapter.bluetoothLeAdvertiser
+
+        bluetoothLeAdvertiser?.let {
+            val settings = AdvertiseSettings.Builder()
+                .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_BALANCED)
+                .setConnectable(true)
+                .setTimeout(0)
+                .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM)
+                .build()
+
+            val data = AdvertiseData.Builder()
+                .setIncludeDeviceName(true)
+                .setIncludeTxPowerLevel(false)
+                .addServiceUuid(ParcelUuid(TimeProfile.TIME_SERVICE))
+                .build()
+
+            it.startAdvertising(settings, data, advertiseCallback)
+        } ?: Log.w(TAG, "Failed to create advertiser")
+    }
+
+    fun stopAdvertising() {
+        val bluetoothLeAdvertiser: BluetoothLeAdvertiser? =
+            bluetoothManager.adapter.bluetoothLeAdvertiser
+        bluetoothLeAdvertiser?.let {
+            it.stopAdvertising(advertiseCallback)
+        } ?: Log.w(TAG, "Failed to create advertiser")
+    }
+
+    fun startServer() {
+        bluetoothGattServer = bluetoothManager.openGattServer(this, gattServerCallback)
+
+        bluetoothGattServer?.addService(TimeProfile.createTimeService()) ?: Log.w(TAG, "Unable to create GATT server")
+    }
+
+    fun stopServer() {
+        bluetoothGattServer?.close()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.i(TAG, "Starting gatt server")
+
+        bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        val bluetoothAdapter = bluetoothManager.adapter
+        if(!checkBluetoothSupport(bluetoothAdapter)) {
+            Log.e(TAG, "bluetooth not supported")
+        }
+
+        val filter = IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED)
+        registerReceiver(bluetoothReceiver, filter)
+        if (!bluetoothAdapter.isEnabled) {
+            Log.d(TAG, "Bluetooth is currently disabled...enabling")
+            bluetoothAdapter.enable()
+        } else {
+            Log.d(TAG, "Bluetooth enabled...starting services")
+            startAdvertising()
+            startServer()
+        }
+
+        return START_STICKY
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+
+
+    /**
+     * Verify the level of Bluetooth support provided by the hardware.
+     * @param bluetoothAdapter System [BluetoothAdapter].
+     * @return true if Bluetooth is properly supported, false otherwise.
+     */
+    private fun checkBluetoothSupport(bluetoothAdapter: BluetoothAdapter?): Boolean {
+
+        if (bluetoothAdapter == null) {
+            Log.w(TAG, "Bluetooth is not supported")
+            return false
+        }
+
+        if (!packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Log.w(TAG, "Bluetooth LE is not supported")
+            return false
+        }
+
+        return true
+    }
+}
--- app/src/main/java/ca/berlingoqc/growbe_android_module/SettingsActivity.kt
+++ app/src/main/java/ca/berlingoqc/growbe_android_module/SettingsActivity.kt
@@ -1,8 +1,15 @@
 package ca.berlingoqc.growbe_android_module
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
+import androidx.core.app.ActivityCompat
+
+
+
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -16,6 +23,21 @@ class SettingsActivity : AppCompatActivity() {
                     .commit()
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        validatePermission()
+
+        val intent = Intent(this, GattServerService::class.java)
+        startService(intent)
+    }
+
+
+    private fun validatePermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED)  {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.BLUETOOTH_CONNECT), 1)
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_ADVERTISE) != PackageManager.PERMISSION_GRANTED)  {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.BLUETOOTH_ADVERTISE), 1)
+        }
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
